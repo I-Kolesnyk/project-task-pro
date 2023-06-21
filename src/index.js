2d25008909c041c8ff727c2cbc0bcd5919d9dab6
I-Kolesnyk <106455256+I-Kolesnyk@users.noreply.github.com>
2023-06-21 03:00:27 +0300

    adds theme

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Global, ThemeProvider } from '@emotion/react';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { GlobalStyles } from 'styles/GlobalStyles';
+import { theme } from 'styles/Theme.styled';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme.dark}>
+    <Global styles={GlobalStyles} />
     <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
