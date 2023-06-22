a002f631e4294500acde5d1180c2075d8e5458c0
marinamelnik1105 <masha.nasha119@gmail.com>
2023-06-23 00:11:34 +0300

    add restricted route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { theme } from 'styles/Theme.styled';
 import { useTheme } from 'hooks';
 import ScreenPage from 'components/ScreenPage/ScreenPage';
 import Layout from 'components/Layout/Layout';
+// import PrivateRoute from 'components/PrivateRoute';
+// import RestrictedRoute from 'components/RestrictedRoute';
+
 
 const HomePage = lazy(() => import('pages/HomePage'));
 const WelcomePage = lazy(() => import('pages/WelcomePage'));
@@ -19,8 +22,15 @@ function App()
     <ThemeProvider theme={theme[themeMode]}>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
+
+          {/* <RestrictedRoute path="/welcome" > */}
           <Route path="/welcome" element={<WelcomePage />} />
+          {/* </RestrictedRoute> */}
+
+          {/* <RestrictedRoute path="/auth/:id" > */}
           <Route path="/auth/:id" element={<AuthPage />} />
+          {/* </RestrictedRoute> */}
+
           {/* <PrivateRoute path="/home"> */}
           <Route path="/home" element={<Layout />}>
             <Route index element={<HomePage />} />
--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,4 +1,4 @@
-// import { Route } from "react-router-dom";
+// import { Navigate, Route } from "react-router-dom";
 
 // const  PrivateRoute = ({ children, ...routeProps }) =>
 // {
