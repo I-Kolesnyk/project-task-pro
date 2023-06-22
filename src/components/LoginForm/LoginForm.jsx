947b96d7f862980c67a55cddf1fc7ad8086259fc
unknown <vlad.vtorygin@gmail.com>
2023-06-22 21:36:13 +0300

    Add button for form auth and propTypes. Add styles for Links welcom page

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,5 +1,12 @@
+import AuthBtn from 'components/AuthBtn';
+
 const LoginForm = () => {
-  return <div>Login Form</div>;
+  return (
+    <div>
+      Login Form
+      <AuthBtn textBtn="Log In Now" />
+    </div>
+  );
 };
 
 export default LoginForm;
