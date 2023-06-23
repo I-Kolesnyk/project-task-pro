390c81339b4ead6e6b4fbf015529e72bee81824d
Petro Psekho <psekho.pp@gmail.com>
2023-06-23 10:00:19 +0300

    add useLocation to AuthTabs

--- src/components/AuthTabs/AuthTabs.jsx
+++ src/components/AuthTabs/AuthTabs.jsx
@@ -1,16 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { AuthTabsContainer } from './AuthTabs.styled';
 import LoginForm from 'components/LoginForm/LoginForm';
 import RegisterForm from 'components/RegisterForm/RegisterForm';
 
 const AuthTabs = () => {
   const [activeTab, setActiveTab] = useState(0);
+  const location = useLocation();
 
   const handleTabChange = index => {
     setActiveTab(index);
   };
+
+  useEffect(() => {
+    const { pathname } = location;
+    if (pathname === '/auth/register') {
+      setActiveTab(0);
+    } else if (pathname === '/auth/login') {
+      setActiveTab(1);
+    }
+  }, [location]);
 
   return (
     <AuthTabsContainer>
