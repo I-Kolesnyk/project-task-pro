e71e455b2c93b871d0a0169ea17e73e90ee03954
Petro Psekho <psekho.pp@gmail.com>
2023-06-24 15:45:54 +0300

    add modal window container

---
+++ src/components/ModalWindow/ModalWindow.styled.jsx
@@ -0,0 +1,30 @@
+import styled from '@emotion/styled';
+
+export const ModalOverlay = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: rgba(0, 0, 0, 0.4);
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ModalContent = styled.div`
+  position: relative;
+  padding: 40px;
+  border-radius: 8px;
+  background-color: #151515;
+`;
+
+export const CloseButton = styled.svg`
+  position: absolute;
+  top: 14px;
+  right: 14px;
+  width: 18px;
+  height: 18px;
+  stroke: white;
+  cursor: pointer;
+`;
