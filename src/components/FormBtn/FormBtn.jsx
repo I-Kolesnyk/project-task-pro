3c122ff5be1b072bf592a71b847b991a0e73d42b
unknown <vlad.vtorygin@gmail.com>
2023-06-24 20:12:09 +0300

    fix form btn and login text

--- src/components/FormBtn/FormBtn.jsx
+++ src/components/FormBtn/FormBtn.jsx
@@ -3,9 +3,11 @@ import { BtnForm } from './FormBtn.styled';
 function FormBtn({ textBtn }) {
   return (
     <>
-      <BtnForm type="submit">{textBtn}</BtnForm>
+      <BtnForm type="submit">{textBtn()}</BtnForm>
     </>
   );
 }
 
 export default FormBtn;
+
+//example for FormBtn <FormBtn textBtn={() => <ChildComponent textContent="Add" />} />
