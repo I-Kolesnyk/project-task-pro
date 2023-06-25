8ea9e9d443f5dd265c4decfb9d62382941acc8b4
Eugene Hlushak <hlushak.e@gmail.com>
2023-06-25 12:50:05 +0300

    add welcome component for mobile devices

---
+++ src/components/WelcomeImg/WelcomeImg.styled.jsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const Img = styled.img`
+  margin-right: auto;
+  margin-left: auto;
+  margin-bottom: 14px;
+`;
--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,9 +1,10 @@
-
+import Welcome from 'components/Welcome/Welcome';
 import { StyledLink } from './styled/WelcomePage.styled';
 
 const WelcomePage = () => {
   return (
     <div>
+      <Welcome />
       <StyledLink to="/auth/register">Registration</StyledLink>
 
       <StyledLink to="/auth/login">Log In</StyledLink>
