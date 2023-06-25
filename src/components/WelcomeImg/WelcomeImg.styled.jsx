6b50fbde48f9f864e784031d03a7035b1ef1546f
Eugene Hlushak <hlushak.e@gmail.com>
2023-06-25 14:57:05 +0300

    add adaptive layout

--- src/components/WelcomeImg/WelcomeImg.styled.jsx
+++ src/components/WelcomeImg/WelcomeImg.styled.jsx
@@ -1,7 +1,15 @@
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 
-export const Img = styled.img`
+const dynamicImgStyles = ({ theme }) => css`
   margin-right: auto;
   margin-left: auto;
   margin-bottom: 14px;
+  @media only screen and (min-width: ${theme.breakpoints[1]}) {
+    margin-bottom: 24px;
+  }
+`;
+
+export const Img = styled.img`
+  ${dynamicImgStyles}
 `;
