e148036c442483baa9d581308d6047555266f272
Iryna Kolesnyk <i.kolesnyk.goit@gmail.com>
2023-06-26 04:21:01 +0300

    fix theme

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -3,4 +3,5 @@ export const selectIsLoading = state => state.auth.isLoading;
 export const selectUserName = state => state.auth.user.name;
 export const selectUserAvatar = state => state.auth.user.avatar;
 export const selectUserId = state => state.auth.user.id;
+export const selectTheme = state => state.auth.theme;
 export const selectIsFetchingCurrentUser = state => state.auth.isFetchingCurrentUser;
