8f7a315a85ffb9b7da7a8f6be653fafb7f07f4e3
Petro Psekho <psekho.pp@gmail.com>
2023-06-27 14:11:28 +0300

    add bg to welcomr page

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,14 +1,16 @@
 import Welcome from 'components/Welcome/Welcome';
-import { StyledLink } from './styled/WelcomePage.styled';
+import { WelcomePageContainer, StyledLink } from './styled/WelcomePage.styled';
 
 const WelcomePage = () => {
   return (
-    <div>
-      <Welcome />
-      <StyledLink to="/auth/register">Registration</StyledLink>
+    <WelcomePageContainer>
+      <div>
+        <Welcome />
+        <StyledLink to="/auth/register">Registration</StyledLink>
 
-      <StyledLink to="/auth/login">Log In</StyledLink>
-    </div>
+        <StyledLink to="/auth/login">Log In</StyledLink>
+      </div>
+    </WelcomePageContainer>
   );
 };
 
