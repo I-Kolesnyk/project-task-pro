4008c83f1fa16f9db06663dc11c32d30fb5d778f
Nazarenko Alex <nazarenko.alx@gmail.com>
2023-06-29 01:12:14 +0300

    adds anchor text for homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,32 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router';
 import { getAllBoards } from 'redux/allBoards/operations';
 import { useAllBoards } from 'hooks';
 import { getBoardById } from 'redux/board/operations';
+import {
+  HomePageContainer,
+  HomePageText,
+  HomePageCreateBoardBtn,
+} from './styled/HomePage.styled';
+import Modal from 'components/ModalWindow/ModalWindow';
+import CreateNewBoard from 'components/NewBoardForm/NewBoardForm';
 
 function HomePage() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const allBoards = useAllBoards();
   const activeBoard = allBoards.filter(board => board.active === true);
-  console.log(activeBoard);
+
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
   useEffect(() => {
     if (activeBoard.length !== 0 && allBoards.length !== 0) {
@@ -22,7 +38,27 @@ function HomePage() {
     }
   }, [activeBoard, allBoards, dispatch, navigate]);
 
-  return <p>Homepage</p>;
+  return (
+    <>
+      <HomePageContainer>
+        <HomePageText>
+          Before starting your project, it is essential&nbsp;
+          <HomePageCreateBoardBtn onClick={openModal}>
+            to create a board
+          </HomePageCreateBoardBtn>
+          &nbsp; to visualize and track all the necessary tasks and milestones.
+          This board serves as a powerful tool to organize the workflow and
+          ensure effective collaboration among team members.
+        </HomePageText>
+      </HomePageContainer>
+
+      {isModalOpen && (
+        <Modal isOpen={isModalOpen} onClose={closeModal}>
+          <CreateNewBoard onClose={closeModal} />
+        </Modal>
+      )}
+    </>
+  );
 }
 
 export default HomePage;
