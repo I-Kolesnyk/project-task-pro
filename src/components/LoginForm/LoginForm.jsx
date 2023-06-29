630df75e57f0d237002f445be8422d2a7ce188af
Iryna Kolesnyk <i.kolesnyk.goit@gmail.com>
2023-06-30 00:55:20 +0300

    Update LoginForm.jsx

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -10,27 +10,6 @@ import { Form, Input, ShowPassword, Svg } from './LoginForm.styled';
 
 import sprite from '../../assets/sprite.svg';
 
-const schema = yup
-  .object({
-    email: yup
-      .string()
-      .required('Email is required')
-      .matches(
-        '^([A-Za-z0-9_-]+.)*[A-Za-z0-9_-]+@[a-z0-9_-]+(.[a-z0-9_-]+)*.[a-z]{2,6}$',
-        'Invalid email format'
-      ),
-    password: yup
-      .string()
-      .required('Password is required')
-      .min(8)
-      .max(64)
-      .matches(
-        /^[A-Za-z0-9!@#$%^&*()_+=\-[\]{}|\\:;"'<>,.?/~`]+$/,
-        'Invalid password format'
-      ),
-  })
-  .required();
-
 const LoginForm = () => {
   const dispatch = useDispatch();
 
