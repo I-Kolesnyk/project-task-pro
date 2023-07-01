a8a5de54ce519a31173abedc3a7d3bcb9e074ed6
Iryna Kolesnyk <i.kolesnyk.goit@gmail.com>
2023-07-01 16:57:05 +0300

    adds messages

--- src/services/axios.js
+++ src/services/axios.js
@@ -47,6 +47,7 @@ axiosPrivateFormData.interceptors.request.use(
   async config => {
     const user = localStorage.getItem('persist:auth');
     const parsedUser = JSON.parse(user);
+    console.log(parsedUser);
     const token = parsedUser.token.slice(1, -1);
     if (token) {
       if (config?.headers) {
@@ -62,18 +63,88 @@ axiosPrivateFormData.interceptors.request.use(
   }
 );
 
-// axiosPublic.interceptors.response.use(async error => {
-//   if (!error.response) {
-//     toast.error('Please check your internet connection and try again!');
-//   }
-//   if (
-//     error.response.status === 404 &&
-//     error.config.url === '/api//auth/login'
-//   ) {
-//     toast.error('User is not found. Please sign up!');
-//   }
-//   if (error.response.status === 409 && error.config.url === '/auth/register') {
-//     toast.error('User with such email is already exist.');
-//   }
-//   return Promise.reject(error);
-// });
+axiosPublic.interceptors.response.use(
+  response => response,
+  async error => {
+    console.log(error);
+    if (error.response.status === 500) {
+      toast.error('Something has happened. Please try again later.');
+    }
+    if (error.response.status === 400) {
+      toast.error('Something has happened. Please report us an error!');
+    }
+    if (
+      error.response.status === 404 &&
+      error.config.url === '/api/users/login'
+    ) {
+      toast.error('Email or password is incorrect');
+    }
+    if (
+      error.response.status === 409 &&
+      error.config.url === '/api//users/register'
+    ) {
+      toast.error('User with such email is already exist.');
+    }
+    return Promise.reject(error);
+  }
+);
+
+axiosPrivateFormData.interceptors.response.use(
+  async response => {
+    console.log(response);
+    if ((response.data.status = 200)) {
+      toast.success('User data were successfully updated');
+      return response;
+    }
+    return response;
+  },
+  async error => {
+    console.log(error);
+    if (error.response.status === 500) {
+      toast.error('Something has happened. Please try again later.');
+    }
+    if (error.response.status === 401) {
+      return (window.location.href = '/project-task-pro/auth/login');
+    }
+    if (error.response.status === 400) {
+      toast.error('Something has happened. Please report us an error!');
+    }
+    if (error.response.status === 409) {
+      toast.error('User with such email is already exist.');
+    }
+    return Promise.reject(error);
+  }
+);
+
+axiosPrivateJson.interceptors.response.use(
+  async response => {
+    console.log(response);
+    if (response.data.code === 201 && response.config.url === '/api/boards') {
+      toast.success('New board was successfully added');
+      return response;
+    }
+    if (response.data.status === 200 && response.config.url === '/api/email') {
+      toast.success(
+        'Your question was sent. We will answer your as soon as posiible.'
+      );
+      return response;
+    }
+    return response;
+  },
+  async error => {
+    console.log(error);
+    if (error.response.status === 500) {
+      toast.error('Something has happened. Please try again later.');
+    }
+    if (error.response.status === 401) {
+      return (window.location.href = '/project-task-pro/auth/login');
+    }
+    if (error.response.status === 400) {
+      toast.error('Something has happened. Please report us an error!');
+    }
+    if (error.response.status === 409 && error.config.url === '/api/boards') {
+      toast.error('A board with this name already exists');
+    }
+    return Promise.reject(error);
+  }
+);
