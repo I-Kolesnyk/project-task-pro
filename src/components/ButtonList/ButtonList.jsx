56bddaf5ec832cf79175ef99da49a6a336dcb421
Eugene Hlushak <hlushak.e@gmail.com>
2023-07-01 11:46:48 +0300

    add edit

--- src/components/ButtonList/ButtonList.jsx
+++ src/components/ButtonList/ButtonList.jsx
@@ -5,57 +5,10 @@ import { List } from './ButtonListStyled';
 function ButtonList() {
   const allBoards = useAllBoards();
 
-  let allBoards2 = {
-    boards: [
-      {
-        _id: '64996aeb200e2e9a4078415e',
-        title: 'Test board',
-        icon: '',
-        active: true,
-        owner: '64996677200e2e9a40783df1',
-      },
-      {
-        _id: '64996aeb200e2e9a4078e45e',
-        title: 'Second board',
-        icon: '',
-        active: false,
-        owner: '64996677200e2e9a40783df1',
-      },
-      {
-        _id: '64996aeb200e2e9a4078415e',
-        title: 'Just board',
-        icon: '',
-        active: false,
-        owner: '64996677200e2e9a40783df1',
-      },
-      {
-        _id: '64996aeb200e2e9a4078415e',
-        title: 'Just board',
-        icon: '',
-        active: false,
-        owner: '64996677200e2e9a40783df1',
-      },
-      {
-        _id: '64996aeb200e2e9a4078415e',
-        title: 'Just board',
-        icon: '',
-        active: false,
-        owner: '64996677200e2e9a40783df1',
-      },
-      {
-        _id: '64996aeb200e2e9a4078415e',
-        title: 'Just board',
-        icon: '',
-        active: false,
-        owner: '64996677200e2e9a40783df1',
-      },
-    ],
-  };
-
   return (
     <List>
-      {allBoards2.boards.length !== 0 &&
-        allBoards2.boards.map(({ _id, title, icon, active }) => (
+      {allBoards.boards.length !== 0 &&
+        allBoards.boards.map(({ _id, title, icon, active }) => (
           <BoardButton
             key={_id}
             name={title}
