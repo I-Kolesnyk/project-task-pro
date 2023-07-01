5accf58789aa7610de6a31bf955c28d4314b0595
Nazarenko Alex <nazarenko.alx@gmail.com>
2023-07-02 00:26:13 +0300

    adds background slice

---
+++ src/redux/background/slice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { getBackgrounds } from './operations';
+
+const initialState = {
+  backgrounds: [],
+  isLoading: false,
+};
+
+export const backgroundsSlice = createSlice({
+  name: 'backgrounds',
+  initialState,
+  reducers: {},
+  extraReducers: builder =>
+    builder
+      .addCase(getBackgrounds.fulfilled, (state, action) => {
+        state.backgrounds = action.payload.data;
+        state.isLoading = false;
+      })
+      .addCase(getBackgrounds.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(getBackgrounds.rejected, (state, action) => {
+        state.isLoading = false;
+      }),
+});
